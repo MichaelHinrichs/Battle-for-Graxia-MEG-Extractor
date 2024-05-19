fe9ae81c2779f860ef9b186b6f31284043009cb2
NintenHero <37460517+MichaelHinrichs@users.noreply.github.com>
2024-05-19 14:34:57 -0500

    Add more games to "Written for" comment

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿//Written for games that use Megafiles.
 //Command & Conquer™ Remastered Collection https://store.steampowered.com/app/1213210
+//8-Bit Hordes https://store.steampowered.com/app/497850
+//9-Bit Armies: A Bit Too Far https://store.steampowered.com/app/1439750
+//STAR WARS™ Empire at War - Gold Pack https://store.steampowered.com/app/32470
 //Battle for Graxia https://steamcommunity.com/app/90530
 //Victory Command https://steamcommunity.com/app/360480
 using System.Collections.Generic;
